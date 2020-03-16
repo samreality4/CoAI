0b06d739c0cef79a3d3fbceb3525738ff41ab6e0
sam <samreality4@gmail.com>
2020-03-16 14:46:48 -0700

    notes 3.16.20

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+//parse data from http post request
 const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 
-//axios posts json
+//support parsing of json.
 app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
+//merge and normalize(conforms to the operating system) the given path arguments.
 
 app.use(
   session({
@@ -25,13 +28,14 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
-
+//to alter the req object and change the user value in the current session to a deserialized user object.
 
 mongoose.set("useCreateIndex", true);
 const conn1 = mongoose.createConnection(process.env.MONGO_KEY1, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+//old methods were deprecated and replaced by these new methods
 
 conn1.on("error", console.error.bind(console, "connection err:"));
 conn1.once("open", () => {
@@ -60,17 +64,19 @@ userSchema.plugin(passportLocalMongoose);
 const User = conn1.model("User", userSchema);
 
 passport.use(User.createStrategy());
+//use username and password to authenticate users
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
-
+//save user object in session during login
 passport.deserializeUser((id, done) => {
   User.findById(id, (err, user) => {
     console.log(err);
     done(err, user);
   });
 });
+//find user by id and then attaches the user object to req
 
 //Code Collection Related//
 
@@ -92,8 +98,8 @@ codeSchema.index({
 
 const Code = conn2.model("Code", codeSchema);
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 require("./routes/searchRoutes")(app, Code);
