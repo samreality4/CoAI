e51dbff969995538578f624f6f1ded9cf4354a8b
sam <samreality4@gmail.com>
2020-03-16 14:54:26 -0700

    updated Home

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -11,38 +11,38 @@ function Home(props) {
       return (
         <div className="container mw-100">
           <div className="row">
-          <header
-            className="text-white col-sm-12"
-            style={{
-              height:"19rem",
-              backgroundImage: `url(${"/images/banner.jpg"})`
-            }}
-          >
-            <Zoom in={true}>
-              <div className="row col-md-6 text-center m-auto p-5">
-                <h1>
-                  Hi! Welcome to CoAI! A helper for any coder! Use it today!
-                </h1>
-              </div>
-            </Zoom>
-          </header>
-          <div>
-            <div className="col-sm-12">
+            <header
+              className="text-white col-sm-12"
+              style={{
+                height: "19rem",
+                backgroundImage: `url(${"/images/banner.jpg"})`
+              }}
+            >
               <Zoom in={true}>
-                <div className="row col-md-6 m-auto p-5">
-                  <h1>About CoAI</h1>
-                  <p>
-                    CoAI is a personal helper tool for any coder out there that
-                    needs extra solution recalling. It searches solution codes
-                    based on saved keywords, titles, and project name. Don't let
-                    those hours of stackflowing and googling go to waste. Store
-                    them in CoAI now!
-                  </p>
+                <div className="row col-md-6 text-center m-auto p-5">
+                  <h1>
+                    Hi! Welcome to CoAI! A helper for any coder! Use it today!
+                  </h1>
                 </div>
               </Zoom>
+            </header>
+            <div>
+              <div className="col-sm-12">
+                <Zoom in={true}>
+                  <div className="row col-md-6 m-auto p-5">
+                    <h1>About CoAI</h1>
+                    <p>
+                      CoAI is a collaborative helper tool for any coder out
+                      there that needs some extra help. It searches solution
+                      codes based on saved keywords, titles, and project name
+                      shared by you and other users. Store your solution in CoAI
+                      now!
+                    </p>
+                  </div>
+                </Zoom>
+              </div>
             </div>
           </div>
-        </div>
         </div>
       );
   }
